051c215aa90e7562d06ac6a54c7096dd994a7f8a
Karol-Regula <kregula@stuy.edu>
2016-10-05 01:14:46 -0400

    Fixed directory structure, made utils and data folders.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import hashlib
+from utils import authenticate
 
 app = Flask(__name__)
 
@@ -11,31 +11,7 @@ def render ():
 
 @app.route("/authenticate/", methods = ['POST']) #only post requests allowed
 def auth():
-    #print request.headers
-    if (request.form['action'] == 'Login'):
-        credentials = open('credentials.csv', 'r')
-        for line in credentials:
-            comma = line.find(',')
-            print line[comma + 1:]
-            print hashlib.sha224(request.form['password']).hexdigest()
-            if (request.form['user'] == line[:comma] and hashlib.sha224(request.form['password']).hexdigest() + '\n' == line[comma + 1:]):
-                return render_template('authentication.html', result = "Login successful")
-            if (request.form['user'] == line[:comma] and not hashlib.sha224(request.form['password']).hexdigest() + '\n' == line[comma + 1:]):
-                return render_template('form.html', message = "Incorrect password")
-        return render_template('form.html', message = "Username does not exist")
-    else: #registering
-        credentials = open('credentials.csv', 'r')
-        duplicate = False
-        for line in credentials:
-            comma = line.find(',')
-            if (request.form['user'] == line[:comma]):
-                duplicate = True
-        if (duplicate):
-            return render_template('form.html', message = "Username already exists")
-        else:
-            credentials = open('credentials.csv', 'a')
-            credentials.write(request.form['user'] + ',' + hashlib.sha224(request.form['password']).hexdigest() + '\n')
-            return render_template('form.html', message = "Registration Successful!")
+    return authenticate.auth(request.form['user'], request.form['password'], request.form['action'])
 
 if __name__ == '__main__':
     app.debug = True
